0e35888461359ffc1edc82e0857ae551267b4c3a
nathan <nathan.poncet.pro@gmail.com>
2024-07-16 16:07:50 +0200

    ✨ export realms doc

--- server/src/graphql_logic/context.rs
+++ server/src/graphql_logic/context.rs
@@ -10,17 +10,13 @@ impl GraphQLContext {
     pub fn keycloak_auth(&self) -> KeycloakAuth<AlwaysReturnPolicy> {
         let keycloak_pk = std::env::var("KEYCLOAK_PK").expect("KEYCLOAK_PK not found in .env file");
         let keycloak_pk = format!("-----BEGIN PUBLIC KEY-----\n{keycloak_pk}\n-----END PUBLIC KEY-----");
-        
-        let res = KeycloakAuth {
+
+        KeycloakAuth {
             detailed_responses: true,
             passthrough_policy: AlwaysReturnPolicy,
             keycloak_oid_public_key: DecodingKey::from_rsa_pem(keycloak_pk.as_bytes()).unwrap(),
             required_roles: vec![],
         }
-
-        println!("{:?}", res.);
-
-        res
     }
 }
 
