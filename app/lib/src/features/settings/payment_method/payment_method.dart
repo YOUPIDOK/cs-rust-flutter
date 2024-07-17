65a6e9cec8cf9aba448421509858bdd2890b0f87
Amine Fajry <amine.f@decq.com>
2024-07-17 17:28:34 +0200

    feat: add boutton for payment methiod

--- app/lib/src/features/settings/payment_method/payment_method.dart
+++ app/lib/src/features/settings/payment_method/payment_method.dart
@@ -6,10 +6,11 @@ class PaymentMethod extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Votre moyen de paiement')),
-        body: const _PaymentMethodState(),
-      );
+      appBar: AppBar(title: const Text('Votre moyen de paiement')),
+      body: const _PaymentMethodState(),
+    );
   }
+
 }
 
 class _PaymentMethodState extends StatefulWidget {
@@ -21,32 +22,49 @@ class _PaymentMethodState extends StatefulWidget {
 
 class _PaymentMethodStateState extends State<_PaymentMethodState> {
   String _selectedMethod = 'card';
+  List<String> cards = ['XXXX-XXXX-XXXX-1234'];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.credit_card),
-              title: Text('XXXX-XXXX-XXXX-1234'),
-              subtitle: Text('Expire le 12/24'),
-            ),
-            RadioListTile<String>(
-              title: const Text('Utiliser cette carte'),
-              value: 'card',
-              groupValue: _selectedMethod,
-              onChanged: (String? value) {
-                setState(() {
-                  _selectedMethod = value!;
-                });
-              },
-            ),
-          ],
+    return Stack(
+      children: [
+        ListView.builder(
+          itemCount: cards.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.credit_card),
+                    title: Text(cards[index]),
+                    subtitle: const Text('Expire le 12/24'),
+                  ),
+                  RadioListTile<String>(
+                    title: const Text('Utiliser cette carte'),
+                    value: 'card',
+                    groupValue: _selectedMethod,
+                    onChanged: (String? value) {
+                      setState(() {
+                        _selectedMethod = value!;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
         ),
-      ),
+        Positioned(
+          bottom: 90,
+          right: 20,
+          child: FloatingActionButton(
+            onPressed: () {
+            },
+            child: const Icon(Icons.add),
+          ),
+        ),
+      ],
     );
   }
 }
