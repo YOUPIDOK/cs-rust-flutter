ca0e9ce3ec339474329118a6cb54fce48df99ede
YOUPIDOK <58979097+YOUPIDOK@users.noreply.github.com>
2024-07-17 11:48:13 +0200

    Db user

--- server/migrations/2024-07-15-143136_create_users/up.sql
+++ server/migrations/2024-07-15-143136_create_users/up.sql
@@ -1,10 +1,19 @@
+CREATE TYPE USER_GENDER as ENUM ('MALE', 'FEMALE', 'OTHER');
+
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-    firstname VARCHAR NOT NULL,
-    lastname VARCHAR NOT NULL,
-    email VARCHAR NOT NULL,
     keycloak_uuid UUID NOT NULL,
+    email UUID NOT NULL,
+    firstname VARCHAR NOT NULL,
+    gender USER_GENDER NOT NULL,
+    lastname VARCHAR NOT NULL,
+    country VARCHAR(50) NOT NULL,
+    address VARCHAR NOT NULL,
+    postal_code VARCHAR(10) NOT NULL,
+    mobile_number VARCHAR(20) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    authorized_data_sell BOOLEAN NOT NULL DEFAULT FALSE
 );
+
 SELECT diesel_manage_updated_at('users');
