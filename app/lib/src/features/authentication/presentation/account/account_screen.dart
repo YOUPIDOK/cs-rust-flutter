be22c06a835d844abe970a14467981be26e2f6c4
nalvac <vacna1997@gmail.com>
2024-07-18 20:14:53 +0200

    Profile page updated

--- app/lib/src/features/authentication/presentation/account/account_screen.dart
+++ app/lib/src/features/authentication/presentation/account/account_screen.dart
@@ -1,105 +1,126 @@
-import 'package:app/src/constants/app_sizes.dart';
-import 'package:app/src/constants/paddings.dart';
-import 'package:app/src/features/authentication/data/auth_repository.dart';
-import 'package:app/src/routing/app_router.dart';
+
+import 'package:app/src/features/authentication/presentation/account/payment_method/account_payment_method_screen.dart';
+import 'package:app/src/features/authentication/presentation/account/personal_information/account_personal_information_screen.dart';
+import 'package:app/src/features/authentication/presentation/account/review_history/account_review_history_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class AccountScreen extends ConsumerWidget {
   const AccountScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final logout = ref.watch(authRepositoryProvider).signOut;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Account'),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment(1, 1),
-            colors: <Color>[
-              Color.fromRGBO(144, 39, 142, 0.8),
-              Color.fromRGBO(3, 144, 235, 0.8),
-            ],
+  Widget build(BuildContext context,  WidgetRef ref) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Paramètres')),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment(1, 1),
+              colors: <Color>[
+                Color.fromRGBO(144, 39, 142, 0.8),
+                Color.fromRGBO(3, 144, 235, 0.8),
+              ],
+            ),
           ),
-        ),
-        padding: Paddings.page,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () => context.goNamed(AppRoute.accountPersonnalInfo.name),
-                child: const SizedBox(
-                  width: double.infinity,
-                  height: 60,
-                  child: ListTile(
-                    leading: Icon(Icons.face),
-                    title: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Information personnel'),
+          child: Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) =>
+                              const AccountPersonalInformationScreen()),
+                        );
+                      },
+                      child: const SizedBox(
+                        width: double.infinity,
+                        height: 60,
+                        child: ListTile(
+                          leading: Icon(Icons.face),
+                          title: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text('Information'),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            gapH12,
-            Card(
-              shape: RoundedRectangleBorder(side: const BorderSide(color: Colors.grey, width: 1), borderRadius: BorderRadius.circular(5)),
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () => context.goNamed(AppRoute.accountReviewHistory.name),
-                child: const SizedBox(
-                  width: double.infinity,
-                  height: 60,
-                  child: ListTile(
-                    leading: Icon(Icons.hotel_class),
-                    title: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Historique des avis'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      side: const BorderSide(color: Colors.grey, width: 1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => const AccountPaymentMethodScreen()),
+                        );
+                      },
+                      child: const SizedBox(
+                        width: double.infinity,
+                        height: 60,
+                        child: ListTile(
+                          leading: Icon(Icons.credit_card),
+                          title: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text('Paiement'),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ),
-            gapH12,
-            Card(
-              shape: RoundedRectangleBorder(
-                side: const BorderSide(color: Colors.grey, width: 1),
-                borderRadius: BorderRadius.circular(5),
-              ),
-              clipBehavior: Clip.hardEdge,
-              child: InkWell(
-                splashColor: Colors.blue.withAlpha(30),
-                onTap: () => context.goNamed(AppRoute.accountPaymentMethod.name),
-                child: const SizedBox(
-                  width: double.infinity,
-                  height: 60,
-                  child: ListTile(
-                    leading: Icon(Icons.credit_card),
-                    title: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text('Paiement'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                        side: const BorderSide(color: Colors.grey, width: 1),
+                        borderRadius: BorderRadius.circular(5)),
+                    clipBehavior: Clip.hardEdge,
+                    child: InkWell(
+                      splashColor: Colors.blue.withAlpha(30),
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                              builder: (context) => const AccountReviewHistoryScreen()),
+                        );
+                      },
+                      child: const SizedBox(
+                        width: double.infinity,
+                        height: 60,
+                        child: ListTile(
+                          leading: Icon(Icons.hotel_class),
+                          title: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text('Historique des avis'),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ),
-              ),
+              ],
             ),
-            gapH12,
-            OutlinedButton(onPressed: logout, child: const Text('Logout')),
-          ],
+          ),
         ),
       ),
     );
--- app/lib/src/features/authentication/presentation/account/payment_method/account_payment_method_screen.dart
+++ app/lib/src/features/authentication/presentation/account/payment_method/account_payment_method_screen.dart
@@ -1,4 +1,4 @@
-import 'package:app/src/constants/paddings.dart';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
@@ -36,7 +36,8 @@ class _AccountPaymentMethodScreenStateState extends State<AccountPaymentMethodSc
               primary: Colors.blue,
               secondary: Colors.blue,
             ),
-            buttonTheme: const ButtonThemeData(textTheme: ButtonTextTheme.primary),
+            buttonTheme:
+            const ButtonThemeData(textTheme: ButtonTextTheme.primary),
           ),
           child: child!,
         );
@@ -51,70 +52,44 @@ class _AccountPaymentMethodScreenStateState extends State<AccountPaymentMethodSc
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Payment Method'),
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment(1, 1),
-            colors: <Color>[
-              Color.fromRGBO(144, 39, 142, 0.8),
-              Color.fromRGBO(3, 144, 235, 0.8),
-            ],
-          ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment(1, 1),
+          colors: <Color>[
+            Color.fromRGBO(144, 39, 142, 0.8),
+            Color.fromRGBO(3, 144, 235, 0.8),
+          ],
         ),
-        child: Padding(
-          padding: Paddings.page,
-          child: Stack(
-            children: [
-              ListView(
-                children: [
-                  ...cards.map((card) {
-                    return Card(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          ListTile(
-                            leading: const Icon(Icons.credit_card),
-                            title: Text(card['number']!),
-                            subtitle: Text('Expire le ${card['expiry']}'),
-                            trailing: IconButton(
-                              icon: const Icon(Icons.delete),
-                              onPressed: () {
-                                setState(() {
-                                  cards.remove(card);
-                                });
-                              },
-                            ),
-                          ),
-                          RadioListTile<String>(
-                            title: const Text('Utiliser cette carte'),
-                            value: card['number'] ?? '',
-                            groupValue: _selectedMethod,
-                            onChanged: (String? value) {
-                              setState(() {
-                                _selectedMethod = value!;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    );
-                  }),
-                  const Divider(),
-                  Card(
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Stack(
+          children: [
+            ListView(
+              children: [
+                ...cards.map((card) {
+                  return Card(
                     child: Column(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
-                        const ListTile(
-                          title: Text('Communication des données'),
+                        ListTile(
+                          leading: const Icon(Icons.credit_card),
+                          title: Text(card['number']!),
+                          subtitle: Text('Expire le ${card['expiry']}'),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              setState(() {
+                                cards.remove(card);
+                              });
+                            },
+                          ),
                         ),
                         RadioListTile<String>(
-                          title: const Text('Autoriser la commercialisation de vos données personnelles comme moyen de paiement pour utiliser notre application'),
-                          value: 'data_sharing',
+                          title: const Text('Utiliser cette carte'),
+                          value: card['number'] ?? '',
                           groupValue: _selectedMethod,
                           onChanged: (String? value) {
                             setState(() {
@@ -124,87 +99,114 @@ class _AccountPaymentMethodScreenStateState extends State<AccountPaymentMethodSc
                         ),
                       ],
                     ),
+                  );
+                }),
+                const Divider(),
+                Card(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(
+                        title: Text('Communication des données'),
+                      ),
+                      RadioListTile<String>(
+                        title: const Text(
+                            'Autoriser la commercialisation de vos données personnelles comme moyen de paiement pour utiliser notre application'),
+                        value: 'data_sharing',
+                        groupValue: _selectedMethod,
+                        onChanged: (String? value) {
+                          setState(() {
+                            _selectedMethod = value!;
+                          });
+                        },
+                      ),
+                    ],
                   ),
-                  if (_isAddingNewCard)
-                    Card(
-                      child: Container(
-                        padding: const EdgeInsets.all(16.0),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10.0),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              spreadRadius: 2,
-                              blurRadius: 5,
-                              offset: const Offset(0, 3), // changes position of shadow
+                ),
+                if (_isAddingNewCard)
+                  Card(
+                    child: Container(
+                      padding: const EdgeInsets.all(16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Column(
+                        children: [
+                          TextField(
+                            controller: _cardController,
+                            decoration: const InputDecoration(
+                              labelText: 'Nouveau numéro de carte',
+                              border: OutlineInputBorder(),
                             ),
-                          ],
-                        ),
-                        child: Column(
-                          children: [
-                            TextField(
-                              controller: _cardController,
-                              decoration: const InputDecoration(
-                                labelText: 'Nouveau numéro de carte',
-                                border: OutlineInputBorder(),
+                            keyboardType: TextInputType.number,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(16),
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          TextField(
+                            readOnly: true,
+                            decoration: InputDecoration(
+                              labelText: _selectedExpiryDate == null
+                                  ? 'Date d\'expiration'
+                                  : 'Expire le ${DateFormat('MM/yy').format(_selectedExpiryDate!)}',
+                              border: const OutlineInputBorder(),
+                              suffixIcon: IconButton(
+                                icon: const Icon(Icons.calendar_today),
+                                onPressed: () => _selectExpiryDate(context),
                               ),
-                              keyboardType: TextInputType.number,
-                              inputFormatters: [
-                                FilteringTextInputFormatter.digitsOnly,
-                                LengthLimitingTextInputFormatter(16),
-                              ],
                             ),
-                            const SizedBox(height: 10),
-                            TextField(
-                              readOnly: true,
-                              decoration: InputDecoration(
-                                labelText: _selectedExpiryDate == null ? 'Date d\'expiration' : 'Expire le ${DateFormat('MM/yy').format(_selectedExpiryDate!)}',
-                                border: const OutlineInputBorder(),
-                                suffixIcon: IconButton(
-                                  icon: const Icon(Icons.calendar_today),
-                                  onPressed: () => _selectExpiryDate(context),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(height: 10),
-                            ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  if (_cardController.text.isNotEmpty && _selectedExpiryDate != null) {
-                                    final formattedDate = DateFormat('MM/yy').format(_selectedExpiryDate!);
-                                    cards.add({
-                                      'number': _cardController.text,
-                                      'expiry': formattedDate,
-                                    });
-                                    _cardController.clear();
-                                    _selectedExpiryDate = null;
-                                    _isAddingNewCard = false;
-                                  }
-                                });
-                              },
-                              child: const Text('Ajouter la carte'),
-                            ),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(height: 10),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                if (_cardController.text.isNotEmpty &&
+                                    _selectedExpiryDate != null) {
+                                  final formattedDate = DateFormat('MM/yy')
+                                      .format(_selectedExpiryDate!);
+                                  cards.add({
+                                    'number': _cardController.text,
+                                    'expiry': formattedDate,
+                                  });
+                                  _cardController.clear();
+                                  _selectedExpiryDate = null;
+                                  _isAddingNewCard = false;
+                                }
+                              });
+                            },
+                            child: const Text('Ajouter la carte'),
+                          ),
+                        ],
                       ),
                     ),
-                ],
+                  ),
+              ],
+            ),
+            Positioned(
+              bottom: 90,
+              right: 20,
+              child: FloatingActionButton(
+                onPressed: () {
+                  setState(() {
+                    _isAddingNewCard = true;
+                  });
+                },
+                child: const Icon(Icons.add),
               ),
-              Positioned(
-                bottom: 90,
-                right: 20,
-                child: FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      _isAddingNewCard = true;
-                    });
-                  },
-                  child: const Icon(Icons.add),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
