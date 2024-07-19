89a9053ca4012bea809225f3ad2916d9a432a1e4
RobinCoblentz <82441417+RobinCoblentz@users.noreply.github.com>
2024-07-19 10:56:54 +0200

    add paywall

--- app/lib/nfc_await_outside.dart
+++ app/lib/nfc_await_outside.dart
@@ -3,6 +3,76 @@ import 'nfc_await_inside.dart';
 import 'error_page.dart'; // Assurez-vous d'importer votre fichier error_page.dart
 
 class NfcAwaitOutside extends StatelessWidget {
+  final bool payment;
+
+  NfcAwaitOutside({this.payment = true });
+
+  void _showPleaseWaitDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Veuillez patienter'),
+          content: const Text('Chargement en cours'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fermer le dialog
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NfcAwaitInside()),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _showConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirmation d\'achat'),
+          content: const Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Vous avez renseigné votre carte bleue, et payez avec celle-ci. Si vous souhaitez utiliser l'application gratuitement, vous pouvez commercialiser vos données via les paramètres de votre profil.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 14), // Réduire la taille du texte
+              ),
+              SizedBox(height: 10),
+              Text(
+                "Prix: 1.99€",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fermer le dialog de confirmation
+              },
+            ),
+            TextButton(
+              child: const Text('Confirmer'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fermer le dialog de confirmation
+                _showPleaseWaitDialog(context); // Afficher le dialog "Veuillez patienter"
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,27 +82,11 @@ class NfcAwaitOutside extends StatelessWidget {
           children: <Widget>[
             GestureDetector(
               onTap: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Veuillez patienter'),
-                      content: const Text('Chargement en cours'),
-                      actions: <Widget>[
-                        TextButton(
-                          child: const Text('OK'),
-                          onPressed: () {
-                            Navigator.of(context).pop(); // Fermer le dialog
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => NfcAwaitInside()),
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
+                if (payment) {
+                  _showConfirmationDialog(context);
+                } else {
+                  _showPleaseWaitDialog(context);
+                }
               },
               child: Image.asset(
                 'assets/contactless.png', // Assurez-vous d'ajouter votre logo dans ce répertoire
@@ -56,7 +110,7 @@ class NfcAwaitOutside extends StatelessWidget {
                   MaterialPageRoute(builder: (context) => ErrorPage()),
                 );
               },
-              child: const Text('Accès apage de test Erreur (a supprimer)'),
+              child: const Text('Accès à la page de test Erreur (à supprimer)'),
             ),
           ],
         ),
